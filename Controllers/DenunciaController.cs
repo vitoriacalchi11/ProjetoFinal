7e4810b21ea2ea221b5e86b6581390aa34179387
Vitoria <vitoriacalchi@gmail.com>
2024-11-14 07:32:27 -0300

    lastProject

---
+++ Controllers/DenunciaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoFloresta.Data;
+using ProjetoFloresta.Models;
+
+namespace ProjetoFloresta.Controllers
+{
+    public class DenunciaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DenunciaController()
+        {
+            _context = new ApplicationDbContext(); // Instância do contexto do Entity Framework
+        }
+
+        // GET: Formulario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Formulario/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DenunciaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Adiciona o objeto model ao contexto do banco de dados
+                    _context.DenunciaModel.Add(model);
+                    _context.SaveChanges(); // Salva as alterações no banco de dados
+
+                    TempData["SuccessMessage"] = "Dados salvos com sucesso!";
+                    return RedirectToAction("Index"); // Redireciona para uma página, como a lista de registros
+                }
+                catch (Exception ex)
+                {
+                    // Log do erro para fins de depuração
+                    Console.WriteLine("Erro ao salvar os dados: " + ex.Message);
+                    ModelState.AddModelError("", "Erro ao salvar os dados. Tente novamente.");
+                }
+            }
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+}
---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ProjetoFloresta.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+       
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public PerfilController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = await _userManager.FindByIdAsync(userId);
+                ViewData["UserName"] = User.Identity.Name;
+                //ViewData["Nome"] = User.FindFirst(ClaimTypes.Nome)?.Value;
+                //ViewData["Telefone"] = User.FindFirst(ClaimTypes.Telefone)?.Value;
+                ViewData["Email"] = User.FindFirst(ClaimTypes.Email)?.Value;
+                return View();
+            }
+            else
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
+
+        }
+
+        
+
+    }
+}
